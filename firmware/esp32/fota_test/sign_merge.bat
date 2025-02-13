1115fe5f3cec7dcb175a32c63251c7c8b985db9d
wmd1942 <47285745+wmd1942@users.noreply.github.com>
2025-02-13 18:23:30 -0500

    Delete firmware/esp32/fota_test/sign_merge.bat

--- firmware/esp32/fota_test/sign_merge.bat
+++
@@ -1,7 +0,0 @@
-REM Sign the binary with private key
-openssl dgst -sign priv_key.pem -keyform PEM -sha256 -out esp32_fota.bin.sign -binary esp32_fota.ino.esp32.bin
-
-REM Merge signature and binary files together
-copy esp32_fota.bin.sign/B + esp32_fota.ino.esp32.bin/B esp32_fota.img/B
-
-del esp32_fota.bin.sign
